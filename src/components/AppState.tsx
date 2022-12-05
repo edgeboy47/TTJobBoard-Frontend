4a6ce96be003315f306533b01ab75a5e53253950
Darrion Mills <dmills1994@gmail.com>
2022-12-05 13:01:15 -0400

    feat: add basic search by job title or company name

--- src/components/AppState.tsx
+++ src/components/AppState.tsx
@@ -1,5 +1,6 @@
 import { useRef, useCallback, useEffect, useState } from "react";
 import { ApiResponse } from "../pages";
+import { useDebounce } from "../utils/hooks";
 import JobList from "./JobList";
 import SearchBar from "./Searchbar";
 
@@ -14,6 +15,39 @@ const AppState = ({ data: initialJobs, meta: initialMeta }: ApiResponse) => {
   const [meta, setMeta] = useState<ApiResponse["meta"]>(initialMeta);
   const [searchOptions, setSearchOptions] = useState<SearchOptions>({});
 
+  // Debounce the user input for search, to reduce api calls
+  const debouncedSearchOptions = useDebounce<SearchOptions>(searchOptions);
+
+  // TODO: move data fetching logic to SWR
+  // Data fetching logic
+  const fetchJobs = useCallback(
+    async ({ page, title, company }: SearchOptions & { page?: number }) => {
+      const params = new URLSearchParams();
+      let url: string;
+
+      if (title) params.append("title", title);
+      if (page) params.append("page", page.toString());
+      if (company) params.append("company", company);
+
+      // TODO: check if object is empty or deep equal to {}
+      if (params.toString()) url = `/api/jobs?${params.toString()}`;
+      else url = "/api/jobs";
+
+      const body = await fetch(url);
+
+      const json = await body.json();
+
+      // If scrolling to another page, concat the data instead of replacing it
+      if (page) {
+        setJobs((prev) => prev.concat(json.data));
+      } else {
+        setJobs(json.data);
+      }
+      setMeta(json.meta);
+    },
+    [setJobs, setMeta]
+  );
+
   // Ref for intersection observer div
   const observerRef = useRef<HTMLDivElement>(null);
 
@@ -22,11 +56,16 @@ const AppState = ({ data: initialJobs, meta: initialMeta }: ApiResponse) => {
     async (entries) => {
       const [target] = entries;
       if (target.isIntersecting && meta.currentPage < meta.totalPages) {
-        const res = await fetch(`/api/jobs?page=${meta.currentPage + 1}`);
-        const body = await res.json();
+        // TODO: move data fetching to SWR
+        // const res = await fetch(`/api/jobs?page=${meta.currentPage + 1}`);
+        // const body = await res.json();
 
-        setJobs((prev) => prev.concat(body.data));
-        setMeta(body.meta);
+        // setJobs((prev) => prev.concat(body.data));
+        // setMeta(body.meta);
+        await fetchJobs({
+          ...debouncedSearchOptions,
+          page: meta.currentPage + 1,
+        });
       }
     },
     [meta.currentPage, meta.totalPages, setJobs, setMeta]
@@ -34,15 +73,20 @@ const AppState = ({ data: initialJobs, meta: initialMeta }: ApiResponse) => {
 
   // Setup intersection observer
   useEffect(() => {
-    const el = observerRef.current!;
+    const el = observerRef.current;
     const observer = new IntersectionObserver(handleObserver, {
       threshold: 0,
     });
 
-    observer.observe(el);
+    if (el) observer.observe(el);
 
     return () => observer.disconnect();
   }, [handleObserver]);
+
+  // Handle search logic whenever searchOptions changes
+  useEffect(() => {
+    fetchJobs({ ...debouncedSearchOptions });
+  }, [debouncedSearchOptions]);
 
   return (
     <div className="max-w-5xl mx-auto my-0">
--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 
-export function useDebounce<T>(value: T, delay?: number): T {
+// Found on usehooks.com
+// T is a generic type for value parameter, our case this will be string
+export function useDebounce<T>(value: T, delay: number = 500): T {
+  // State and setters for debounced value
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [value, delay]);
-
+  useEffect(
+    () => {
+      // Update debounced value after delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
   return debouncedValue;
 }
