bbc8b60bce90d5a29b39c5cdc96660d6b4f8c0c1
Darrion Mills <dmills1994@gmail.com>
2025-04-11 21:31:01 -0400

    chore: setup storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import storybook from "eslint-plugin-storybook"
+import { FlatCompat } from '@eslint/eslintrc'
 
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  ...storybook.configs['flat/recommended'],
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
 ];
