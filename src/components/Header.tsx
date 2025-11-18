af1fbb3b939a759febf43eca5db3ff258038803a
Darrion Mills <dmills1994@gmail.com>
2025-11-17 22:08:03 -0400

    feat: add flag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { TT } from 'country-flag-icons/react/3x2'
 
 const Header = () => {
   return (
     <header className="w-full bg-gray-50 rounded-md shadow flex justify-center">
-      <div className='p-6 max-w-5xl w-full'>
-        <h1 className='text-5xl font-semibold'>TT Job Board</h1>
+      <div className='flex flex-row p-6 max-w-5xl w-full gap-4 items-center'>
+        <h1 className='text-5xl font-semibold'>TT Job Board  </h1>
+        <TT className='w-12 h-12' />
       </div>
     </header>
   );
