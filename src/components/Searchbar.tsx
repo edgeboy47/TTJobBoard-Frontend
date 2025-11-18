59bc1a07d167db32c27f40ca47e0abcb8356fa00
Darrion Mills <dmills1994@gmail.com>
2025-11-18 09:55:10 -0400

    chore: use Card component for Searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { SearchOptions } from "../utils/types";
 import { Label } from './ui/label';
 import { Input } from './ui/input';
+import { Card, CardContent } from './ui/card';
 import { SearchIcon } from 'lucide-react';
 
 type SearchProps = {
@@ -13,44 +14,46 @@ type SearchProps = {
 const SearchBar = ({ searchOptions, setSearchOptions }: SearchProps) => {
   const { title, company } = searchOptions;
   return (
-    <div className="bg-white rounded-md p-8 shadow-md my-6 flex flex-wrap flex-col md:flex-row gap-6">
-      <div className='flex flex-col gap-3 flex-1'>
-        <Label htmlFor="title">Title</Label>
-        <Input
-          icon={<SearchIcon size={16} className='opacity-50' />}
-          className="py-2 rounded-md outline-none shadow"
-          type="text"
-          name="title"
-          id="title"
-          placeholder="Job Title"
-          value={title ?? ""}
-          onChange={(e) =>
-            setSearchOptions((prevOptions) => ({
-              ...prevOptions,
-              title: e.target.value,
-            }))
-          }
-        />
-      </div>
-      <div className='flex flex-col gap-3 flex-1'>
-        <Label htmlFor="company">Company</Label>
-        <Input
-          icon={<SearchIcon size={16} className='opacity-50' />}
-          className="py-1 rounded-md outline-none shadow"
-          type="text"
-          name="company"
-          id="company"
-          placeholder="Company"
-          value={company ?? ""}
-          onChange={(e) =>
-            setSearchOptions((prevOptions) => ({
-              ...prevOptions,
-              company: e.target.value,
-            }))
-          }
-        />
-      </div>
-    </div>
+    <Card className="bg-white rounded-md p-8 shadow-md my-6">
+      <CardContent className="px-0 flex flex-col flex-wrap md:flex-row gap-6">
+        <div className='flex flex-col gap-3 flex-1'>
+          <Label htmlFor="title">Title</Label>
+          <Input
+            icon={<SearchIcon size={16} className='opacity-50' />}
+            className="py-2 rounded-md outline-none shadow"
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Job Title"
+            value={title ?? ""}
+            onChange={(e) =>
+              setSearchOptions((prevOptions) => ({
+                ...prevOptions,
+                title: e.target.value,
+              }))
+            }
+          />
+        </div>
+        <div className='flex flex-col gap-3 flex-1'>
+          <Label htmlFor="company">Company</Label>
+          <Input
+            icon={<SearchIcon size={16} className='opacity-50' />}
+            className="py-1 rounded-md outline-none shadow"
+            type="text"
+            name="company"
+            id="company"
+            placeholder="Company"
+            value={company ?? ""}
+            onChange={(e) =>
+              setSearchOptions((prevOptions) => ({
+                ...prevOptions,
+                company: e.target.value,
+              }))
+            }
+          />
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
