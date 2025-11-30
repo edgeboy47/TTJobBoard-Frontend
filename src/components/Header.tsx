749a0b1eafa64f7de7f29126266821d28e9fd5b2
Darrion Mills <dmills1994@gmail.com>
2025-11-30 17:42:16 -0400

    feat: add transition to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import * as motion from 'motion/react-client'
 import { TT } from 'country-flag-icons/react/3x2'
 
 const Header = () => {
   return (
     <header className="w-full bg-gray-50 rounded-md shadow flex justify-center">
-      <div className='flex flex-row p-6 max-w-5xl w-full gap-4 items-center'>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className='flex flex-row p-6 max-w-5xl w-full gap-4 items-center'>
         <h1 className='text-5xl font-semibold'>TT Job Board  </h1>
         <TT className='w-12 h-12' />
-      </div>
+      </motion.div>
     </header>
   );
 };
