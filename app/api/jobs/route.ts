b605c9378ba26e3ce6ff2ce0ea62ac48251e9586
Darrion Mills <dmills1994@gmail.com>
2025-12-14 22:07:12 -0400

    fix: fix envs

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -3,13 +3,13 @@ import { NextRequest } from "next/server";
 export const dynamic = 'force-dynamic'
 export async function GET(request: NextRequest) {
   const params = request.nextUrl.searchParams
-  const url = `${process.env.NEXT_PUBLIC_API_URL! + '/jobs?' + params}`
+  const url = `${process.env.API_URL! + '/jobs?' + params}`
 
   console.log('fetching from url', url)
   const response = await fetch(url, {
     cache: 'no-store',
     headers: {
-      'x-api-key': process.env.NEXT_PUBLIC_API_KEY!,
+      'x-api-key': process.env.API_KEY!,
     }
   });
 
